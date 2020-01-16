8931d83f2b85f6fee11674f846560a922eb46515
Kah Gene Leong <kgleong2001@gmail.com>
2020-01-16 02:57:31 +0000

    Update files (2)

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-Original source code is from: https://gist.github.com/mbostock/7607535
-License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+# Data Visualization Programming Summative 
+
+Type of visualization used: Zoomable Circle Packing (Original source code: https://gist.github.com/mbostock/7607535)
+Dataset used: UNICEF Reports 2018
+
+## Getting Started
+
+The entire visualization is encapusulated in a class called ZoomableCircle with 4 user-input based parameters for the constructor. These parameters are url (url to dataset), mar (margin), diam (diameter), cR (colour range). The format for url and cR are shown in the jsdoc. 
+
+An example to create the visualization is as so:
+
+```
+const DataVis = new ZoomableCircle('https://raw.githubusercontent.com/KGene1901/DataVis_Programming/master/unicef_data.json', 5, 1500, ['hsl(200,100%,90%)', 'hsl(700,50%,70%)']);
+```
+This can directly be called from the html file with a script tag. However, in my visualization it has already been called. 
+
+There is also an option for users to update the dataset in the visualization with their own dataset with the setter function Updatedata(data). Look at the notes on jsdoc on how to call it.
+
+### Examples
+
+Below are two json files I have provided for testing of the visualization:
+
+```
+'https://raw.githubusercontent.com/KGene1901/DataVis_Programming/master/unicef_data.json'
+```
+```
+'https://gist.githubusercontent.com/mbostock/7607535/raw/863bb1f3dcc8f104f1b52318f6ee927d5c54d678/flare.json'
+```
+
+## Versioning
+
+I used 'version 1.1' of svg ("http://www.w3.org/2000/svg") and v4 of d3 ("https://d3js.org/d3.v4.min.js"). 
+
+## Acknowledgement
+
+README.md-template: https://gist.github.com/PurpleBooth/109311bb0361f32d87a2
+
+## License
+
+https://creativecommons.org/licenses/by-nc-sa/4.0/ 
+
+
+
--- zoom_circle.js
+++ zoom_circle.js
@@ -7,18 +7,25 @@
  * @class
  * @classdesc Creates a data visualization based on circle packing with a zoomable function for user interactivity
  */
+// eslint-disable-next-line no-unused-vars
 class ZoomableCircle /** @lends ZoomableCircle.prototype */ {
     /**
-     * Sets inputs for the styling and formatting of the visualization. It also parses data from the given input
+     * Sets inputs for the styling and formatting of the visualization.<br>It also parses data from the given input.
      * @constructor
-     * @param  {url} dat - Link to a webpage containing the dataset
-     * @param  {int} mar - Size of space between the visualization and the svg container
-     * @param  {int} diam - Diameter of a bubble
+    * @param  {url} url - Link to a webpage containing the dataset (The dataset itself should be in a json file with a structure as so: {'name': {string}, 'children':[{'name':{string}, 'children':[{'name':{string}, 'size':{int/float}}, {
+    * 'name':{string}, 'size':{int/float}, ...}] }]})
+     * @param  {integer} mar - Size of space between the visualization and the svg container
+     * @param  {integer} diam - Diameter of a bubble
      * @param  {array} cR - Colour scheme for the visualization [Format of input is in HSL - Hue (0-360), Saturation (0%-100%) and Lightness (0%-100%) in an array] An example of this would be ['hsl(-,-%,-%)', 'hsl(-,-%,-%)'])
+     * @param  {function} this.svg - Selects the svg container for which the visualization is displayed on
+     * @param  {object} this.g - All circles and text append to the svg container
+     * @param  {object} this.color - Sets the colour for the circles
+     * @param  {object} this.pack - Creates a circle packing layout
      */
     constructor(url, mar, diam, cR) {
         this.data = url;
         this.margin = mar;
+        // Error handling for margin
         if (mar) {
             this.margin = mar;
         } else {
@@ -26,6 +33,7 @@ class ZoomableCircle /** @lends ZoomableCircle.prototype */ {
         };
         this.svg = d3.select('svg');
         this.diameter = diam;
+        // Error handling for diameter
         if (diam) {
             this.diameter = diam;
         } else {
@@ -33,6 +41,7 @@ class ZoomableCircle /** @lends ZoomableCircle.prototype */ {
         };
         this.g = this.svg.append('g').attr('transform', 'translate(' + this.diameter / 2 + ',' + this.diameter / 2 + ')');
         this.colourRange = cR;
+        // Error handling for colour range
         if (cR) {
             this.colourRange = cR;
         } else {
@@ -48,37 +57,44 @@ class ZoomableCircle /** @lends ZoomableCircle.prototype */ {
             .padding(2);
 
         const x = this;
+        // this function parses data
         d3.json(this.data, (error, root) => {
             if (error) throw error;
             x.root = d3.hierarchy(root)
                 .sum(function(d) { return d.size; });
             x.focus = root;
-            x.nodes = x.pack(x.root).descendants();
-            x.draw();
+            x.nodes = x.pack(x.root).descendants(); // assigns values to nodes
+            x.draw(); // draw function is called
         });
     }
 
-    /**
-     * Setter used to input new datasets into the same formatted visualization (*Optional function) This can be called using "[variable name].Updatedata = [url to dataset]"
-     * @param {url} data - Link to new dataset
-     */
+/**
+ * Setter used to input new datasets into the same formatted visualization (*Optional function)<br>This can be called using "[variable name].Updatedata = [url to dataset]".
+ * @function
+ * @param {url} data - Link to new dataset
+ */
     set Updatedata (data) {
         this.data = data;
-        const DataVis = new ZoomableCircle(this.data);
+        // eslint-disable-next-line no-unused-vars
+        const DataVis = new ZoomableCircle(this.data); // refreshes visualization with new dataset
     }
 
-    /**
-     * Takes the split data from the dataset and creates zoomable circles or 'bubbles' based on each data. Based on the structure of the json file, each 'name' will create a node while for each 'children' declared, a subnode is created which is shown as a smaller circle embedded in the bigger circle. The function also sets up the background colour of the svg
-     */
+/**
+ * Takes the split data from the dataset and creates zoomable circles or 'bubbles' based on each data. Based on the structure of the json file, each 'name' will create a node while for each 'children' declared, a subnode is created which is shown as a smaller circle embedded in the bigger circle.<br>The function also sets up the background colour of the svg.
+ * @function
+ * @param {object} this.circle - Creates the circle(s)
+ * @param {object} this.text - Creates the text(s) on the circle(s)
+ * @param {object} this.node - Selects all the circles and texts
+ */
     draw() {
         const x = this;
 
         this.circle = this.g.selectAll('circle')
-            .data(this.nodes)
-            .enter().append('circle')
+            .data(this.nodes) //
+            .enter().append('circle') // for each data value, a circle is created
             .attr('class', (d) => { return d.parent ? d.children ? 'node' : 'node node--leaf' : 'node node--root'; })
-            .style('fill', (d) => { return d.children ? this.color(d.depth) : null; })
-            .on('click', function(d) {
+            .style('fill', (d) => { return d.children ? this.color(d.depth) : null; }) // sets colour for layer of circle
+            .on('click', function(d) { // view zooms in upon click
                 if (x.focus !== d) {
                     x.zoom(d);
                     d3.event.stopPropagation();
@@ -87,23 +103,25 @@ class ZoomableCircle /** @lends ZoomableCircle.prototype */ {
 
         this.text = this.g.selectAll('text')
             .data(this.nodes)
-            .enter().append('text')
+            .enter().append('text') // every "name" is assigned to text
             .attr('class', 'label')
             .style('fill-opacity', (d) => { return d.parent === this.root ? 1 : 0; })
             .style('display', (d) => { return d.parent === this.root ? 'inline' : 'none'; })
-            .text(function(d) { return d.data.name; });
+            .text(function(d) { return d.data.name; }); // 'name' is displayed on each circle
 
-        this.node = this.g.selectAll('circle,text');
+        this.node = this.g.selectAll('circle,text'); // every circle with text is a node
         this.svg
             .style('background', this.color(-1))
             .on('click', () => { this.zoom(this.root); });
 
-        this.zoomTo([this.root.x, this.root.y, this.root.r * 2 + this.margin]);
+        this.zoomTo([this.root.x, this.root.y, this.root.r * 2 + this.margin]); // assigns centre of viewport and width of viewport
     }
 
     /**
-     * Zooms into the circle
+     * Zooms into the circle with a set duration. During the transition, the text display on outer node will disappear while the text on the inner node will display.
+     * @function
      * @param  {any} d - Data currently binded to the current element
+     * @param  {function} this.transition - Zooming into circles
      */
     zoom(d) {
         // eslint-disable-next-line no-unused-vars
@@ -121,13 +139,14 @@ class ZoomableCircle /** @lends ZoomableCircle.prototype */ {
             .filter(function(d) { return d.parent === focus || this.style.display === 'inline'; })
             .style('fill-opacity', function(d) { return d.parent === focus ? 1 : 0; })
             .on('start', function(d) { if (d.parent === focus) this.style.display = 'inline'; })
-            .on('end', function(d) { if (d.parent !== focus) this.style.display = 'none'; });
+            .on('end', function(d) { if (d.parent !== focus) this.style.display = 'none'; }); // if previous node not in focus, then the text will not be displayed for that previous node
     }
 
     /**
-     * Pans the "camera" view to the location of the circle which is to be zoomed in on
-     * @param  {function} v - Interpolator between the two views (each taking the coordinates of the centre of the viewport and the width of the viewport) on a 2D plane 
-     */
+     * Pans the "camera" view to the location of the circle which is to be zoomed in on.
+     * @function
+     * @param  {function} v - Interpolator between the two views (each taking the coordinates of the centre of the viewport and the width of the viewport) on a 2D plane
+     * */
     zoomTo(v) {
         const k = this.diameter / v[2];
         this.view = v;
